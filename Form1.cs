6d7484dd07eebfbef0b6aefb245cfaa1dd7caf1a
BijiGitHub <baniyahamburg@gmail.com>
2020-06-25 19:07:51 +0530

    Button Coded

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace GitWindowsFormsApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("It worked");
+        }
     }
 }
